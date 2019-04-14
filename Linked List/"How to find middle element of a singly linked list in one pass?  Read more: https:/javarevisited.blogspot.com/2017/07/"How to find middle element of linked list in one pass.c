3f61ca6357f593b181e17af0e099ea7c7cf7019d
kirtigarg11 <40167262+kirtigarg11@users.noreply.github.com>
2019-04-14 14:51:32 +0530

    Create "How to find middle element of linked list in one pass.c

---
+++ Linked List/"How to find middle element of a singly linked list in one pass?  Read more: https:/javarevisited.blogspot.com/2017/07/"How to find middle element of linked list in one pass.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+void append(struct Node** head_ref, struct Node **tail_ref, int new_data)
+{
+    struct Node* new_node = new Node;
+    new_node->data  = new_data;
+    new_node->next = NULL;
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+/* Function to get the middle of the linked list*/
+int getMiddle(struct Node *head);
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l;
+    cin>>T;
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+    printf("%d
+", getMiddle(head));
+    }
+    return 0;
+}
+
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+}; */
+/* Should return data of middle node. If linked list is empty, then  -1*/
+int getMiddle(Node *head)
+{
+  struct Node* fast_ptr;
+  struct Node* slow_ptr;
+  fast_ptr=head;
+  slow_ptr=head;
+  while(fast_ptr!=NULL && fast_ptr->next!=NULL){
+      slow_ptr=slow_ptr->next;
+      fast_ptr=fast_ptr->next->next;
+  }
+  return slow_ptr->data;
+}
